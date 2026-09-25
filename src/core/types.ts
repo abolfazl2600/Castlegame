5e7ff48b8294b27fc79f0d28b57aa45be48efc5b
Abolfazl Rezaie <abolfazl2600@gmail.com>
2026-09-25 06:02:32 -0700

    Remove unused unit type

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,6 +1,5 @@
 export type TileKind = 'wall' | 'gate' | 'tower' | 'road' | 'cottage' | 'house' | 'manor';
 export type ToolKind = TileKind | 'erase';
-export type UnitKind = 'worker' | 'soldier';
 export type TerrainKind = 'water' | 'shore' | 'plains' | 'river' | 'mountain' | 'forest';
 
 export interface GridCell {
