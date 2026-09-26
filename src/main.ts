24469a3c4e82b028522858c0c08060fbafec32db
Abolfazl Rezaie <abolfazl2600@gmail.com>
2026-09-25 23:15:44 -0700

    feat: add mobile UI foundation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { ThreeGame } from './ThreeGame';
+import { MobileUI } from './ui/MobileUI';
 import './style.css';
 
 const gameRoot = document.getElementById('game');
 if (!gameRoot) throw new Error('Game root was not found');
 
 new ThreeGame(gameRoot);
+new MobileUI();
