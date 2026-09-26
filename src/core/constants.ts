279215588e5d038f0f4d081868e4d136580c9df6
Abolfazl Rezaie <abolfazl2600@gmail.com>
2026-09-25 21:58:52 -0700

    Bump save version for Windmill

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -5,5 +5,5 @@ export const WORLD_WIDTH = WORLD_COLS * TILE_SIZE;
 export const WORLD_HEIGHT = WORLD_ROWS * TILE_SIZE;
 
 export const SAVE_KEY = 'castle-role-save-v1';
-export const SAVE_VERSION = 8;
+export const SAVE_VERSION = 9;
 export const CASTLE_DETAIL_VERSION = 1;
